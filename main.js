c79e0972cfe1006d4b0f4eda5db0833d174e756a
Tanya-Gorecha <121540704+Tanya-Gorecha@users.noreply.github.com>
2023-03-30 18:20:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var a = "zuma";
+var b = "juma";
+console.log(a)
+console.log(b)
+var v = 5;
+var t = 30;
+var join = v + t;
+console.log(join);
+var sub= t-v;
+console.log(sub);
+var div = t/v;
+var mult = v*t;
+console.log(div);
+console.log(mult);
+var hhh= a.concat(b);
+console.log(hhh);
